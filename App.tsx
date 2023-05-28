e3e57da9f1fb754ddc46c4b9abfcafa738373505
Arman Mahmodi <arman_m121@yahoo.com>
2023-05-28 15:05:17 +0330

    add navigation into app.tsx

--- App.tsx
+++ App.tsx
@@ -1,30 +1,12 @@
-import {View, Text, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {useTranslation} from 'react-i18next';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import './i18n';
-
-const LOCALE_PERSISTENCE_KEY = 'language';
+import TabNavigator from './src/navigation/TabNavigator';
 
 export default function App() {
-  const {t, i18n} = useTranslation();
-
-  const onPressLearnMore = async () => {
-    const lang = await AsyncStorage.getItem(LOCALE_PERSISTENCE_KEY);
-    if (lang === 'en') {
-      i18n.changeLanguage('fa');
-      await AsyncStorage.setItem(LOCALE_PERSISTENCE_KEY, 'fa');
-      console.log({lang})
-    } else {
-      i18n.changeLanguage('en');
-      await AsyncStorage.setItem(LOCALE_PERSISTENCE_KEY, 'en');
-      console.log({lang})
-    }
-  };
   return (
-    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-      <Text>{t('text.HELLO')}</Text>
-      <Button onPress={onPressLearnMore} title="change language" color="blue" />
-    </View>
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
   );
 }
